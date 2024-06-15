7c6e1fdd747dfc90fbd837ea85c9b94e11120bb7
Carlos Osuna <127541538+okamixdev@users.noreply.github.com>
2024-06-15 18:51:25 -0500

    Update setupProxy.js

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = app => {
-  const httpProxy = process.env.HTTP_PROXY
-    ? process.env.HTTP_PROXY
+  const httpProxy = process.env.WEBPAGE_URI
+    ? process.env.WEBPAGE_URI
     : 'http://localhost:3001/'
   app.use(createProxyMiddleware('/api', { target: httpProxy }))
 }
