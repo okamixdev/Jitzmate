e5527d24a5e0298404e2948b384ffac5f867f635
Carlos E. Osuna Izquierdo <cosunai@hotmail.com>
2024-06-16 00:41:05 -0500

    changed some settings in server to tweek the html serve

--- server/server.js
+++ server/server.js
@@ -50,12 +50,12 @@ app.use(routes);
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 }
 
 // Serve up static assets (usually on heroku)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // ------------------------------------------------------------------------------------------------
