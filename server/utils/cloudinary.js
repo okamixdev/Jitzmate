baf20fde2a2916e7ee4102cbf3c3b1a681059933
Carlos E. Osuna Izquierdo <cosunai@hotmail.com>
2024-06-16 13:28:35 -0500

    fixes for frontend

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'dsps6unds',
-    api_key: '532313973247466',
-    api_secret: 'c5dVUrVWq-4ePETgHiAcxSFRgAM'
-
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+    // cloud_name: 'dsps6unds',
+    // api_key: '532313973247466',
+    // api_secret: 'c5dVUrVWq-4ePETgHiAcxSFRgAM'
 });
 
 module.exports = cloudinary;
