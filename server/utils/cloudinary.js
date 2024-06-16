80cd71ac5fb1b3b0590a113c34b8df1e360e8de5
Carlos Osuna <127541538+okamixdev@users.noreply.github.com>
2024-06-16 13:29:43 -0500

    Update cloudinary.js

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -5,9 +5,6 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET_KEY,
-    // cloud_name: 'dsps6unds',
-    // api_key: '532313973247466',
-    // api_secret: 'c5dVUrVWq-4ePETgHiAcxSFRgAM'
 });
 
 module.exports = cloudinary;
